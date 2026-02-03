3e7c30e10be84c6343a946580b11d42fdb57eb7c
sryounes <youunes.mr@gmail.com>
2026-02-03 11:19:11 +0100

    Update Pintura and shape dimensions in MainPintura

--- ac4/MainPintura.java
+++ ac4/MainPintura.java
@@ -3,11 +3,11 @@ package ac4;
 public class MainPintura {
     public static void main(String[] args) {
 
-        Pintura pintura = new Pintura(250);
+        Pintura pintura = new Pintura(100);
 
-        forma rectangulo = new Rectangulo(20, 35);
-        forma esfera = new Esfera(15);
-        forma cilindro = new Cilindro(10, 30);
+        forma rectangulo = new Rectangulo(10, 15);
+        forma esfera = new Esfera(5);
+        forma cilindro = new Cilindro(5, 15);
 
         System.out.println("Pintura en " + rectangulo + ": "
                 + pintura.cantidadPintura(rectangulo));
